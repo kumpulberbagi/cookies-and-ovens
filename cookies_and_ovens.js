fb7ab1f1cf1c629019eb06caf22efd4c0af57923
& <pair+ajilantang.yonisetiawan@devbootcamp.com>
2016-10-04 14:05:05 +0700

    cookies and oven

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,57 @@
 //
 //
 // Your code here
+//class cookies
+function random(){
+  return ((Math.random())+1).toFixed(2)
+}
+
+class Kue {
+  constructor(nama,status) {
+    this.status = this.status;
+    this.nama = nama ;
+    this.status = status;
+  }
+  displayName(){
+    console.log("Kue " +this.nama);
+  }
+}
+
+class kacang extends Kue {
+  constructor(nama,waktu,status) {
+    super(nama,waktu,status);
+    this.waktu = 10;
+  }
+  Status(){
+    super.displayName();
+    if (this.status > this.waktu  ) {
+      console.log(`Hangus, telat ${ this.status-this.waktu} menit`);
+    }else if (this.status <= this.waktu && this.status > this.waktu*0.9) {
+      console.log("Matang")
+    }else {
+      console.log(`Mentah, kurang ${this.waktu-this.status} menit`);
+    }
+  }
+}
+
+class keju extends Kue {
+  constructor(nama,waktu,status) {
+    super(nama,waktu,status);
+    this.waktu = 20;
+  }
+  Status(){
+    super.displayName();
+    if (this.status > this.waktu  ) {
+      console.log(`Hangus, telat ${ this.status-this.waktu} menit`);
+    }else if (this.status <= this.waktu && this.status > this.waktu*0.9) {
+      console.log("Matang")
+    }else {
+      console.log(`Mentah, kurang ${this.waktu-this.status} menit`);
+    }
+  }
+}
+var kuekacang = new kacang("kacang",2);
+kuekacang.Status()
+
+var kusekeju = new keju("keju mza",15)
+kusekeju.Status()
